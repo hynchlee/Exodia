6e0cd24a7dea8fbe347334a48865229569248e38
DESKTOP-9AL2P16\user2 <kms6210123@gmail.com>
2023-05-03 18:19:01 +0900

    header파일 생성(임시로 사용할 HeaderTestController.java와 headerTest.jsp도 추가)

---
+++ semi(java)/semiPrj/src/main/java/com/semi/home/controller/HeaderTestController.java
@@ -0,0 +1,19 @@
+package com.semi.home.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class HeaderTestController extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/headerTest.jsp").forward(req, resp);
+	}
+	
+}
