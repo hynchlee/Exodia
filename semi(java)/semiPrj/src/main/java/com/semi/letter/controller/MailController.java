acd5ba7a24cf60296b44dcfb8637c9e5fce46b37
DESKTOP-2UJDIJD\user2 <chul123369@gmail.com>
2023-05-09 21:09:57 +0900

    Revert "receive-letter 작성"
    This reverts commit 7f8aae2679a7ea1d79202b1b42a16f6cfeb69eb4.

--- semi(java)/semiPrj/src/main/java/com/semi/letter/controller/MailController.java
+++ semi(java)/semiPrj/src/main/java/com/semi/letter/controller/MailController.java
@@ -8,13 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/letter/receive")
-public class ReciveLetterController extends HttpServlet{
+@WebServlet("/letter")
+public class MailController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		req.getRequestDispatcher("/WEB-INF/views/letter/receive-letter.jsp").forward(req, resp);
-	
+		req.getRequestDispatcher("/WEB-INF/views/letter/letter.jsp").forward(req, resp);
 	}
-	
 }
