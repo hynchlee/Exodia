7fe3a2b9f3a2fbfcf708ad1928bd9e380ff669b7
DESKTOP-6CN8RA6\user2 <1111>
2023-05-12 20:04:32 +0900

    로그인페이지 만드는중~

---
+++ semi(java)/semiPrj/src/main/java/com/semi/member/controller/MemberLoginController.java
@@ -0,0 +1,28 @@
+package com.semi.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member/login")
+public class MemberLoginController extends HttpServlet{
+	
+	//아이디 찾기 화면
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/member/login.jsp").forward(req, resp);
+	}
+	
+	//아이디 찾기
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	
+	}
+	
+	
+}
