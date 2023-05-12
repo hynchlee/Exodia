1ea2d254ec897052121ed20e3a7746161f06170b
DESKTOP-6CN8RA6\user2 <1111>
2023-05-12 18:27:12 +0900

    비번재발급 페이지

--- semi(java)/semiPrj/src/main/java/com/semi/member/controller/PwdRenewController.java
+++ semi(java)/semiPrj/src/main/java/com/semi/member/controller/PwdRenewController.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/member/idfind")
-public class IdFindController extends HttpServlet{
+@WebServlet("/member/pwdrenew")
+public class PwdRenewController extends HttpServlet{
 	
-	//아이디 찾기 화면
+	//비밀번호 재발급 화면
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/WEB-INF/views/member/idFind.jsp").forward(req, resp);
+		req.getRequestDispatcher("/WEB-INF/views/member/pwdRenew.jsp").forward(req, resp);
 	}
 	
-	//아이디 찾기
+	//비밀번호 재발급
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
