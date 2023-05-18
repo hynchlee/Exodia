f82869450a44f8ee279f4f5756621b87e603c062
DESKTOP-2UJDIJD\user2 <chul123369@gmail.com>
2023-05-18 17:28:34 +0900

    받은 메세지

--- semi(java)/semiPrj/src/main/java/com/semi/letter/controller/ReceiveLetterController.java
+++ semi(java)/semiPrj/src/main/java/com/semi/letter/controller/ReceiveLetterController.java
@@ -14,25 +14,25 @@ import com.semi.letter.vo.LetterVo;
 
 @WebServlet("/letter/receive")
 public class ReceiveLetterController extends HttpServlet{
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		try {
-			LetterService bs = new LetterService();
-			List<LetterVo> voList = bs.letterList();
-			
-			req.setAttribute("voList", voList);
-			req.getRequestDispatcher("/WEB-INF/views/letter/receive-letter.jsp").forward(req, resp);
-			
-		} catch (Exception e) {
-			System.out.println("받은 쪽지 조회중 에러 발생");
-			e.printStackTrace();
-			
-			req.setAttribute("errorMsg", "받은 쪽지 조회 에러");
-			req.getRequestDispatcher("/WEB-INF/views/common/errorPage.jsp").forward(req, resp);
-			
-		}
-	
-	}
+   @Override
+   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+   
+      try {
+         LetterService bs = new LetterService();
+         List<LetterVo> voList = bs.letterList();
+         
+         req.setAttribute("voList", voList);
+         req.getRequestDispatcher("/WEB-INF/views/letter/receive-letter.jsp").forward(req, resp);
+         
+      } catch (Exception e) {
+         System.out.println("받은 쪽지 조회중 에러 발생");
+         e.printStackTrace();
+         
+         req.setAttribute("errorMsg", "받은 쪽지 조회 에러");
+         req.getRequestDispatcher("/WEB-INF/views/common/errorPage.jsp").forward(req, resp);
+         
+      }
+   
+   }
 
 }
--- semi(java)/semiPrj/src/main/java/com/semi/letter/dao/LetterDao.java
+++ semi(java)/semiPrj/src/main/java/com/semi/letter/dao/LetterDao.java
@@ -12,61 +12,60 @@ import com.semi.common.db.JDBCTemplate;
 import com.semi.letter.vo.LetterVo;
 import com.semi.member.vo.MemberVo;
 
-public class MemberDao {
+public class LetterDao {
 
-	public int writeLetter(LetterVo vo, Connection conn, MemberVo loginMember) throws Exception {
-		
-		
-		String sql = "INSERT INTO LETTER (LETTER_NO, SEND_MEMBER_NO, RECEIVE_MEMBER_NO, LETTER_TITLE, LETTER_CONTENT, STATUS) VALUES (SEQ_LETTER_NO.NEXTVAL,?, (SELECT MEMBER_NO FROM MEMBER WHERE ID = ?),?, ?, 'O');"; 
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, loginMember.getMemberNo());
-		pstmt.setString(2, vo.getReceiveMemberNo());
-		pstmt.setString(3, vo.getLetterTitle());
-		pstmt.setString(4, vo.getLetterContent());
-		int result = pstmt.executeUpdate();
-		
-		if(result == 1) {
-			JDBCTemplate.commit(conn);
-		}
-		
-		JDBCTemplate.rollback(conn);
-		
-		JDBCTemplate.close(pstmt);
-		
-		return result;
-	}
+   public int writeLetter(LetterVo vo, Connection conn, MemberVo loginMember) throws Exception {
+      
+      
+      String sql = "INSERT INTO LETTER (LETTER_NO, SEND_MEMBER_NO, RECEIVE_MEMBER_NO, LETTER_TITLE, LETTER_CONTENT, STATUS) VALUES (SEQ_LETTER_NO.NEXTVAL,?, (SELECT MEMBER_NO FROM MEMBER WHERE ID = ?),?, ?, 'O');"; 
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, loginMember.getMemberNo());
+      pstmt.setString(2, vo.getReceiveMemberNo());
+      pstmt.setString(3, vo.getLetterTitle());
+      pstmt.setString(4, vo.getLetterContent());
+      int result = pstmt.executeUpdate();
+      
+      if(result == 1) {
+         JDBCTemplate.commit(conn);
+      }
+      
+      JDBCTemplate.rollback(conn);
+      
+      JDBCTemplate.close(pstmt);
+      
+      return result;
+   }
 
-	public List<LetterVo> letterList(Connection conn) throws Exception {
-		
-		String sql = "SELECT * FROM LETTER L JOIN MEMBER M ON L.SEND_MEMBER_NO = M.MEMBER_NO ORDER BY LETTER_NO";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		ResultSet rs = pstmt.executeQuery();
-		
-		List<LetterVo> voList = new ArrayList<>();
-		while(rs.next()) {
-			String id = rs.getString("MEMBER_ID");
-			String title = rs.getString("LETTER_TITLE");
-			String content = rs.getString("LETTER_CONTENT");
-			String enrollDate = rs.getString("ENROLL_DATE");
-			
-			LetterVo vo = new LetterVo();
-			
-			vo.setSendMemberName(id);
-			vo.setLetterTitle(title);
-			vo.setLetterContent(content);
-			vo.setEnrollDate(enrollDate);
-			
-			voList.add(vo);
-			
-			JDBCTemplate.close(rs);
-			JDBCTemplate.close(pstmt);
-		}
-		
-		return voList;
-	}
+   public List<LetterVo> letterList(Connection conn) throws Exception {
+      
+      String sql = "SELECT * FROM LETTER L JOIN MEMBER M ON L.SEND_MEMBER_NO = M.MEMBER_NO ORDER BY LETTER_NO";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      ResultSet rs = pstmt.executeQuery();
+      
+      List<LetterVo> voList = new ArrayList<>();
+      while(rs.next()) {
+         String id = rs.getString("MEMBER_ID");
+         String title = rs.getString("LETTER_TITLE");
+         String content = rs.getString("LETTER_CONTENT");
+         String enrollDate = rs.getString("ENROLL_DATE");
+         
+         LetterVo vo = new LetterVo();
+         
+         vo.setSendMemberName(id);
+         vo.setLetterTitle(title);
+         vo.setLetterContent(content);
+         vo.setEnrollDate(enrollDate);
+         
+         voList.add(vo);
+      }
+      
+      JDBCTemplate.close(rs);
+      JDBCTemplate.close(pstmt);
+      
+      return voList;
+   }
 
 }
-
 
 
 
